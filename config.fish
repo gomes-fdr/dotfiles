e7d43dcb530d94e0e6f948166011d3a213a5d567
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2018-05-29 19:52:37 -0300

    Update to use at home

--- config.fish
+++ config.fish
@@ -1,3 +1,3 @@
-set -x PATH "/home/frgomes/.pyenv/bin" $PATH
+set -x PATH "/home/fabiano/.pyenv/bin" $PATH
 status --is-interactive; and . (pyenv init -|psub)
 status --is-interactive; and . (pyenv virtualenv-init -|psub)
